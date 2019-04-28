d092f39dea4afb9cc4c2525babbf5b079a782df2
Tanmay Vij <tanmayvij047695@gmail.com>
2019-04-28 13:32:19 +0530

    Sending correct auth token for post requests

--- src/app/apiservice.service.ts
+++ src/app/apiservice.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 
+
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiJ0ZXN0MiIsImxldmVsIjoxLCJpYXQiOjE1NTYyMDU0OTMsImV4cCI6MTU1NjI5MTg5M30.luqbDb-sSLxsX0LKFTaF6NTYDO4tS6kCI8V_d9pizmA'
+    'Authorization': "Bearer " + sessionStorage.token
     })
 };
 
